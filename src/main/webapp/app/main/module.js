24cd0edbe5a74a92c93d44f0ff0008cd249bf323
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-11-25 22:51:26 -0200

    frontend - new application with angular.js 1.2.1, twitter bootstrap 3.0.2 and responsive design reviewed

---
+++ src/main/webapp/app/main/module.js
@@ -0,0 +1,14 @@
+//Declaração da aplicação
+angular.module(
+  // module name
+  'app', 
+
+  // module dependencies
+  [
+    'fend.comp.progressbar.loading', 
+    'fend.comp.input.utils', 
+    'fend.comp.pagination', 
+    'ngRoute',
+    'ngResource'
+  ]
+);
---
+++ src/main/webapp/require.config.js
@@ -0,0 +1,70 @@
+require({
+
+  // libraries dependencies with fallback
+  paths: {
+
+    jquery: [
+      'vendor/jquery/1.10.2/jquery.min'
+    ],
+
+    bootstrap: [
+      'vendor/bootstrap/3.0.2/js/bootstrap.min'
+    ],
+
+    angular: [
+      'vendor/angular.js/1.2.1/angular.min'
+    ],
+
+    angular_route: [
+      'vendor/angular.js/1.2.1/angular-route.min'
+    ],
+
+    angular_resource: [
+      'vendor/angular.js/1.2.1/angular-resource.min'
+    ],
+
+    ngProgress: [
+      'vendor/ngProgress/1.0.3/ngProgress.min'
+    ]
+
+  },
+
+  // define js scripts dependencies
+  shim: {
+
+    'bootstrap': {
+      deps: ['jquery']
+    },
+
+    'angular': {
+      deps: ['bootstrap']
+    },
+
+    'angular_route': {
+      deps: ['angular']
+    },
+
+    'angular_resource': {
+      deps: ['angular']
+    },
+
+    'ngProgress': {
+      deps: ['angular']
+    }
+
+  }
+
+},
+
+['require'], function(require) {
+
+  console.log('project require.js config');
+
+  require([
+    'shared/components/progressbar/loading/require.config',
+    'shared/components/input/utils/require.config',
+    'shared/components/pagination/require.config',
+    'app/require.config'
+  ]);
+
+});
