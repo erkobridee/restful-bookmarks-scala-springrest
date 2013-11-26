2752658160858a369562c7b1fa9e18e95c19cf85
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-11-25 22:26:27 -0200

    backend - bookmarkdao update

--- src/main/scala/com/erkobridee/restful/bookmarks/scala/springrest/persistence/dao/hibernate/BookmarkDAO.scala
+++ src/main/scala/com/erkobridee/restful/bookmarks/scala/springrest/persistence/dao/hibernate/BookmarkDAO.scala
@@ -17,13 +17,21 @@ import javax.persistence.Table
 import javax.xml.bind.annotation.XmlRootElement
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport
 import com.erkobridee.restful.bookmarks.scala.springrest.persistence.dao.TraitBookmarkDAO
+import org.hibernate.criterion.Criterion
+import org.hibernate.Criteria
+import java.lang.Integer
+import com.erkobridee.restful.bookmarks.scala.springrest.persistence.entity.BookmarkResultData
+import com.erkobridee.restful.bookmarks.scala.springrest.persistence.entity.BookmarkResultData
+import com.erkobridee.restful.bookmarks.scala.springrest.persistence.entity.BookmarkResultData
+import java.lang.Integer
+import org.hibernate.criterion.Projections
 
 @Repository("bookmarkDAO")
 class BookmarkDAO extends HibernateDaoSupport with TraitBookmarkDAO {
 
   // --------------------------------------------------------------------------
   
-  val log : Logger = LoggerFactory.getLogger(classOf[Bookmark])
+  val log : Logger = LoggerFactory.getLogger(classOf[BookmarkDAO])
   
   // --------------------------------------------------------------------------
   
@@ -40,32 +48,32 @@ class BookmarkDAO extends HibernateDaoSupport with TraitBookmarkDAO {
   // --------------------------------------------------------------------------
   
   def generateInitData(): Unit = {
-    val list: List[Bookmark] = listAll
+    val bookmarksCount: Int = this.count
     
-    if(list.size() == 0) {
+    if(bookmarksCount == 0) {
       log.debug("generateInitData")
       
       var vo: Bookmark = null
       
-      vo = new Bookmark()
+      vo = new Bookmark
       vo.name = "twitter"
       vo.description = "@ErkoBridee"
       vo.url = "https://twitter.com/ErkoBridee"
       save(vo)
       
-      vo = new Bookmark()
+      vo = new Bookmark
       vo.name = "github"
       vo.description = "github/erkobridee"
       vo.url = "https://github.com/erkobridee"
       save(vo)
       
-      vo = new Bookmark()
+      vo = new Bookmark
       vo.name = "Site"
       vo.description = "Site Erko Bridee"
       vo.url = "http://about.erkobridee.com/"
       save(vo)
 
-      vo = new Bookmark()
+      vo = new Bookmark
       vo.name = "delicious"
       vo.description = "delicious/erko.bridee"
       vo.url = "http://www.delicious.com/erko.bridee"
@@ -75,37 +83,120 @@ class BookmarkDAO extends HibernateDaoSupport with TraitBookmarkDAO {
       
   }
   
-  // --------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  
+  def count(): Int = {
+    this.count(null)
+  }
+  
+  def count(criterion: Criterion): Int = {
+	
+    var c: Criteria = getSession.createCriteria( classOf[Bookmark] )
+	
+    c.setProjection( Projections.rowCount )
+	
+	if(criterion != null) {
+		c.add(criterion)
+	}
+	
+	c.list.get(0).asInstanceOf[Int]	
+  }
+    
+    
+  //----------------------------------------------------------------------------
   
   @Transactional(readOnly = true)
-  def listAll(): List[Bookmark] = 
-    getSession
-      .createCriteria(classOf[Bookmark])
-      .list()
-      .asInstanceOf[List[Bookmark]]
+  def list(): BookmarkResultData = 
+  	this.list( 0, 0 )
 
+  	
   @Transactional(readOnly = true)
-  def findById(id: Long): Bookmark = 
-    getSession
+  def list(page: Int, size: Int = 0): BookmarkResultData = {
+    log.debug("list [ page: " + page + " | size: " + size + " ]")
+    
+    var c: Criteria = getSession.createCriteria(classOf[Bookmark])
+    
+    if(size == 0) {
+    	c.setMaxResults( 10 )
+	} else {
+		c.setMaxResults( size )
+	}
+    
+    if(page > 1) {
+		c.setFirstResult((page-1) * size)
+	} else {
+		c.setFirstResult(0)
+	}
+    
+    new BookmarkResultData(
+        c.list.asInstanceOf[List[Bookmark]],
+        this.count,
+        page, size
+    )
+  }
+  
+  //----------------------------------------------------------------------------
+  
+  @Transactional(readOnly = true)
+  def findById(id: Long): Bookmark = { 
+	log.debug("findById: " + id)
+    
+	getSession
       .get(classOf[Bookmark], Long.box(id))
       .asInstanceOf[Bookmark]
-
+  }
+  
+  //----------------------------------------------------------------------------
+  
   @Transactional(readOnly = true)
-  def findByName(name: String): List[Bookmark] = 
-    getSession
-      .createCriteria(classOf[Bookmark])
-      .add(Restrictions.like("name", "%"+ name + "%" ))
-      .list()
-      .asInstanceOf[List[Bookmark]]
+  def findByName(name: String): BookmarkResultData = 
+    this.findByName(name, 0, 0)
 
+    
+  @Transactional(readOnly = true)
+  def findByName(name : String, page: Int, size: Int): BookmarkResultData = {
+    log.debug("findByName: " + name + "[ page: " + page + " | size: " + size + " ]")
+    
+    val criterion: Criterion = Restrictions.like("name", "%"+ name + "%" )
+    
+    var c: Criteria = getSession.createCriteria(classOf[Bookmark])
+    
+    c.add(criterion)
+    
+    if(size == 0) {
+    	c.setMaxResults( 10 )
+	} else {
+		c.setMaxResults( size )
+	}
+    
+    if(page > 1) {
+		c.setFirstResult((page-1) * size)
+	} else {
+		c.setFirstResult(0)
+	}
+    
+    new BookmarkResultData(
+        c.list.asInstanceOf[List[Bookmark]],
+        this.count(criterion),
+        page, size
+    )
+  }
+  
+  //----------------------------------------------------------------------------
+  
   @Transactional
-  def save(value: Bookmark): Bookmark = 
+  def save(value: Bookmark): Bookmark = {
+    log.debug("save")
+    
     getSession
       .merge(value)
       .asInstanceOf[Bookmark]
+  }
 
   @Transactional
   def remove(id: Long): Boolean = {
+    log.debug("remove: " + id)
+    
     var flag : Boolean = true
     try {
       getSession.delete(findById(id))
