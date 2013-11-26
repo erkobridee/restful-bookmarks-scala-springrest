47e90f382069f7196a717d8804c7c23ab52503a7
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-11-25 22:26:13 -0200

    backend - trait bookmarkdao update

--- src/main/scala/com/erkobridee/restful/bookmarks/scala/springrest/persistence/dao/TraitBookmarkDAO.scala
+++ src/main/scala/com/erkobridee/restful/bookmarks/scala/springrest/persistence/dao/TraitBookmarkDAO.scala
@@ -4,14 +4,24 @@ import java.util.List
 import com.erkobridee.restful.bookmarks.scala.springrest.persistence.entity.Bookmark
 import javax.transaction.Transaction
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport
+import com.erkobridee.restful.bookmarks.scala.springrest.persistence.entity.BookmarkResultData
+import org.hibernate.criterion.Criterion
 
 trait TraitBookmarkDAO {
 
-  def listAll : List[Bookmark]
+  def count : Int
+  
+  def count(criterion: Criterion) : Int
+  
+  def list : BookmarkResultData
+  
+  def list(page: Int, size: Int) : BookmarkResultData
 
   def findById(id: Long) : Bookmark
 
-  def findByName(name : String) : List[Bookmark]
+  def findByName(name : String) : BookmarkResultData
+  
+  def findByName(name : String, page: Int, size: Int) : BookmarkResultData
   
   def save(value : Bookmark) : Bookmark
   
